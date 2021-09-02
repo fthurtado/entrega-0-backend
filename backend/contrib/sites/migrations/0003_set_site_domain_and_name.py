1c15ce0e511d1db0d449562d6ab259743206b072
fede-dealing <fede@dealingapp.com>
2021-09-02 11:26:42 -0400

    feat(): domain

--- backend/contrib/sites/migrations/0003_set_site_domain_and_name.py
+++ backend/contrib/sites/migrations/0003_set_site_domain_and_name.py
@@ -13,7 +13,7 @@ def update_site_forward(apps, schema_editor):
     Site.objects.update_or_create(
         id=settings.SITE_ID,
         defaults={
-            "domain": "entrega-0-backend.com",
+            "domain": "entrega-0-fthurtado.ml",
             "name": "Backend",
         },
     )
--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["entrega-0-backend.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["entrega-0-fthurtado.ml"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
@@ -104,7 +104,7 @@ TEMPLATES[-1]["OPTIONS"]["loaders"] = [  # type: ignore[index] # noqa F405
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
-    "DJANGO_DEFAULT_FROM_EMAIL", default="Backend <noreply@entrega-0-backend.com>"
+    "DJANGO_DEFAULT_FROM_EMAIL", default="Backend <noreply@entrega-0-fthurtado.ml>"
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
