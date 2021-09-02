67f092094b565fcb8ad389340ecc851d875f2d93
fede-dealing <fede@dealingapp.com>
2021-09-02 13:05:03 -0400

    fix(): domain name

--- backend/contrib/sites/migrations/0003_set_site_domain_and_name.py
+++ backend/contrib/sites/migrations/0003_set_site_domain_and_name.py
@@ -13,7 +13,7 @@ def update_site_forward(apps, schema_editor):
     Site.objects.update_or_create(
         id=settings.SITE_ID,
         defaults={
-            "domain": "entrega-0-fthurtado.ml",
+            "domain": "fthurtado-entrega-0.tk",
             "name": "Backend",
         },
     )
